2df2a591721b8d2ffcf0af4385ef9eb9c736516d
Ana Paula Martins <111590785+anapaulama@users.noreply.github.com>
2024-06-18 16:28:19 -0300

    feat: client zod schemas

---
+++ src/client/schemas.ts
@@ -0,0 +1,34 @@
+import z from "zod";
+
+export const clientSchema = z.object({
+  id: z.number().positive(),
+  publicId: z.string(), //para que server este publicID?
+  name: z.string().max(255),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8)
+    .max(50)
+    .regex(/(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])/),
+  birthDate: z.date(),
+  cpf: z.string().max(11),
+  phone: z.string().max(11),
+  storeId: z.number().positive(),
+});
+
+export const clientRegisterSchema = clientSchema.omit({
+  id: true,
+  publicId: true,
+  storeId: true,
+});
+
+export const clientReturnSchema = clientSchema.omit({ password: true });
+
+export const clientLoginSchema = clientSchema.pick({
+  email: true,
+  password: true,
+});
+
+export const clientUpdateSchema = clientSchema
+  .pick({ name: true, email: true, birthDate: true, phone: true })
+  .partial();
