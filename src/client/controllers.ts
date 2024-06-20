142ca1ab380813a0331c7d39df918ce75e32dec7
Ana Paula Martins <111590785+anapaulama@users.noreply.github.com>
2024-06-20 12:55:32 -0300

    feat: client controllers

---
+++ src/client/controllers.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from "express";
+import { inject, injectable } from "tsyringe";
+import { ClientServices } from "./services";
+
+@injectable()
+export class ClientControllers {
+  constructor(@inject(ClientServices) private clientServices: ClientServices) {}
+
+  register = async (req: Request, res: Response): Promise<Response> => {
+    const response = await this.clientServices.register(req.body);
+
+    return res.status(201).json(response);
+  };
+
+  get = async (req: Request, res: Response): Promise<Response> => {
+    const response = await this.clientServices.get();
+
+    return res.status(200).json(response);
+  };
+
+  getOne = async (req: Request, res: Response): Promise<Response> => {
+    const response = await this.clientServices.getOne(Number(req.params.id));
+
+    return res.status(200).json(response);
+  };
+
+  update = async (req: Request, res: Response): Promise<Response> => {
+    const response = await this.clientServices.update(
+      Number(req.params.id),
+      req.body
+    );
+
+    return res.status(200).json(response);
+  };
+
+  remove = async (req: Request, res: Response): Promise<Response> => {
+    await this.clientServices.remove(Number(req.params.id));
+
+    return res.status(204).json();
+  };
+}
