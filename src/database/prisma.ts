be6fec070b94e366fedc033ec4fcdf38185e8db1
matheusnardi <matheusp.nardi@gmail.com>
2024-06-24 14:42:50 +0100

    fix conflit

--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,7 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
-<<<<<<< HEAD
 export const prisma = new PrismaClient();
-=======
-export const prisma = new PrismaClient();
->>>>>>> dd60069c5cb6fdce1fa6a2db3692baeaab998863
+
