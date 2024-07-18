bc5badf69d0a59ad74ad45329b4578548678849e
Danilo Krausz <37805803+dkrausz@users.noreply.github.com>
2024-07-18 11:05:36 +0200

    fixed middleware

--- src/employee/employeeOwner.middleware.ts
+++ src/employee/employeeOwner.middleware.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { prisma } from "../database/prisma";
-import { AppError } from "../@shared/appError";
+import { AppError } from "../@shared/errors";
 
 
 export class EmployeeOwner {
