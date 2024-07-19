d8c67f7e82eddd993c6c158b81e0df3f54215419
Danilo Krausz <37805803+dkrausz@users.noreply.github.com>
2024-07-19 18:56:14 +0200

    Feat add delete product

--- src/products/routes.ts
+++ src/products/routes.ts
@@ -5,7 +5,7 @@ import {  ProductController } from "./controller";
 import { ValidateToken } from "../@shared/validateToken.middleware";
 import { AdmAuth } from "../@shared/admAuth.middleware";
 import { bodyMiddleware } from "../@shared/body.middeware";
-import { createProductBodySchema } from "./schemas";
+import { createProductBodySchema, updateProductSchema } from "./schemas";
 
 
 export const productRoute = Router();
@@ -15,6 +15,8 @@ const productController = container.resolve(ProductController);
 
 productRoute.post("/",ValidateToken.execute,AdmAuth.execute,bodyMiddleware.bodyIsValid(createProductBodySchema),productController.createProduct);
 productRoute.get("/",productController.getProducts);
-productRoute.get("/:id",productController.getOneProduct);
+productRoute.get("/:productId",productController.getOneProduct);
+productRoute.patch("/:productId",ValidateToken.execute,AdmAuth.execute,bodyMiddleware.bodyIsValid(updateProductSchema),productController.updateProduct);
+productRoute.delete("/:productId",ValidateToken.execute,AdmAuth.execute,productController.deleteProduct);
 
 
